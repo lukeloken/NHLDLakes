f1e997ac761c53f8dfcdf2c73ebe1f6a0e9a3523
lukeloken <lukeloken@gmail.com>
2018-02-22 16:15:03 -0600

    added point summaries to loop.

--- R/CompileSpatialSummaries.R
+++ R/CompileSpatialSummaries.R
@@ -9,10 +9,13 @@ library(gtools)
 #### Merge all spatial summaries ####
 
 data_dir<-"E:/Dropbox/FLAME_NHLDlakes/pixelsummaries"
+point_dir<-"E:/Dropbox/FLAME_NHLDlakes/pointsummaries"
 
 spatial_summaries<-list.files(data_dir)
+point_summaries<-list.files(point_dir)
 
 merged_summary<-as.data.frame(matrix(nrow=0, ncol=0))
+merged_points<-merged_summary
 
 summary_file<-spatial_summaries[1]
 for (summary_file in spatial_summaries){
@@ -25,8 +28,6 @@ for (summary_file in spatial_summaries){
   
   summary<-read.csv(paste(data_dir, summary_file, sep="/"), header=T, stringsAsFactors = F)
   summary$range<-summary$Max-summary$Min
-  # str(summary)
-  # head(summary)
   rownames(summary)<-summary$Variable
   transposed_summary<-as.data.frame(t(subset(summary, select = -c(Variable))))
   transposed_summary$Date<-as.Date(rep(date, nrow(transposed_summary)))
@@ -40,13 +41,36 @@ for (summary_file in spatial_summaries){
   else {
     merged_summary<-smartbind(merged_summary, transposed_summary, fill=NA)
   }
+  
+  points<-read.csv(paste(point_dir, summary_file, sep="/"), header=T, stringsAsFactors = F)
+  points$range<-points$Max-points$Min
+  rownames(points)<-points$Variable
+  transposed_points<-as.data.frame(t(subset(points, select = -c(Variable))))
+  transposed_points$Date<-as.Date(rep(date, nrow(transposed_points)))
+  transposed_points$Statistic<-rownames(transposed_points)
+  transposed_points$Lake<-rep(lake, nrow(transposed_points))
+  transposed_points$LakeDay<-rep(lakeday, nrow(transposed_points))
+  
+  if (nrow(merged_points)==0){
+    merged_points<-transposed_points
+  }
+  else {
+    merged_points<-smartbind(merged_points, transposed_points, fill=NA)
+  }
 }
 
 merged_summary$Date<-as.Date(merged_summary$Date)
 rownames(merged_summary)<-c()
 
+merged_points$Date<-as.Date(merged_points$Date)
+rownames(merged_points)<-c()
+
 
 # Save to Git folder
 saveRDS(merged_summary, file='Data/FlameSpatialSummaries.rds')
 write.table(merged_summary, file='Data/FlameSpatialSummaries.csv')
+
+# Save to Git folder
+saveRDS(merged_points, file='Data/FlamePointsSummaries.rds')
+write.table(merged_points, file='Data/FlamePointsSummaries.csv')
 
