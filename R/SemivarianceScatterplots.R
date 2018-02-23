401594e3c1431c0923d01eba8d0446ad8ee7a6d9
lukeloken <lukeloken@gmail.com>
2018-02-22 19:09:03 -0600

    scatterplots of lake area to chla and other semivariogram

--- R/SemivarianceScatterplots.R
+++ R/SemivarianceScatterplots.R
@@ -8,6 +8,11 @@ library(tidyr)
 library(vioplot)
 library(ggplot2)
 
+
+setwd("E:/Git_Repo/NHLDLakes")
+mylagos2<-readRDS(file='Data/MyLakesLagos.rds')
+
+
 setwd("E:/Dropbox/FLAME_NHLDLakes/")
 
 range_model <- readRDS(file='SpatialOutputs/rangemodeltable.rds')
@@ -24,13 +29,14 @@ range_best$flame_name <- gsub(".*_","",range_best$lake_day)
 range_best$flame_name <- gsub("[0-9]","",range_best$flame_name )
 
 
-setwd("E:/Git_Repo/NHLDLakes")
-mylagos2<-readRDS(file='Data/MyLakesLagos.rds')
+
 
 a<-left_join(range_model, mylagos2, by='flame_name')
-
+b<-left_join(range_best, mylagos2, by='flame_name')
 x<-seq(0,max(a$lake_area_ha, na.rm=T)*1.05)
 y<-sqrt(x*10000)
+
+b$Colour <- as.character(cut(b$tp, breaks = seq(0,60,10), labels = brewer.pal(9, "PuBuGn")[3:8]))
 
 #Semivariogram range boxplots
 png("Figures/ScatterplotAreaTempRange.png", res=200, width=4,height=4, units="in")
@@ -59,3 +65,49 @@ mtext('Lake area (ha)',1,2)
 mtext('BGA semivariogram range (m)',2,2)
 
 dev.off()
+
+
+
+#Semivariogram range boxplots withcutoff
+png("Figures/ScatterplotAreaTempRange_withcutoff.png", res=200, width=4,height=4, units="in")
+
+par(mar=c(3,3,.5,.5))
+par(mgp=c(3,.5,0), tck=-.02)
+
+plot(b$lake_area_ha, b$TmpC_h, ylim=c(0,max(y)*1.1), xaxs='i', xlim=c(0, max(x)), pch=16, xlab='', ylab='', col=b$Colour)
+lines(x,y, lty=1)
+text(1100,3500, '~Maximum meaningful range', srt=27, cex=0.8)
+mtext('Lake area (ha)',1,2)
+mtext('Temperature semivariogram range (m)',2,2)
+
+dev.off()
+
+#Semivariogram range boxplots
+png("Figures/ScatterplotAreaBGARange_withcutoff.png", res=200, width=4,height=4, units="in")
+
+par(mar=c(3,3,.5,.5))
+par(mgp=c(3,.5,0), tck=-.02)
+
+plot(b$lake_area_ha, b$BGAPCRFU_h, ylim=c(0,max(y)*1.1), xaxs='i', xlim=c(0, max(x)), pch=16, xlab='', ylab='', col=b$Colour)
+lines(x,y, lty=1)
+text(1100,3500, '~Maximum meaningful range', srt=27, cex=0.8)
+mtext('Lake area (ha)',1,2)
+mtext('BGA semivariogram range (m)',2,2)
+
+dev.off()
+
+
+#Semivariogram range boxplots
+png("Figures/ScatterplotAreachlaRange_withcutoff.png", res=200, width=4,height=4, units="in")
+
+par(mar=c(3,3,.5,.5))
+par(mgp=c(3,.5,0), tck=-.02)
+
+plot(b$lake_area_ha, b$ChlARFU_h, ylim=c(0,max(y)*1.1), xaxs='i', xlim=c(0, max(x)), pch=16, xlab='', ylab='', col=b$Colour)
+lines(x,y, lty=1)
+text(1100,3500, '~Maximum meaningful range', srt=27, cex=0.8)
+mtext('Lake area (ha)',1,2)
+mtext('Chl a semivariogram range (m)',2,2)
+
+dev.off()
+
