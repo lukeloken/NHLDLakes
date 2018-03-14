399f7185bab27fb2e7b0f55b440bac298b60c4fc
lukeloken <lukeloken@gmail.com>
2018-03-14 09:20:04 -0500

    Added minimum values to add to all observations. Also added SDL to summary table

--- R/SpatialStatsPoints_Loop.R
+++ R/SpatialStatsPoints_Loop.R
@@ -39,6 +39,9 @@ filenames<-filenames[-grep('TenderfootCreek', filenames)]
 projection = "+init=epsg:3071"
 # projection = "+proj=utm +zone=15 ellps=WGS84"
 
+#Table of minimums to add to all values to get ride of negatives
+minvars<-data.frame(variable=c("ChlARFU", "ChlAugL", "BGAPCRFU", "BGAPCgL", "TurbFNU", "fDOMRFU", "fDOMQSU"), minimum=c(.12, .84, .21, .19, .42, .62, 1.5)*(-1))
+
 # Transform lakebase and outline into UTM's. This way distance is in meters (m)
 lakes_Base<-spTransform(lakes_Base_UTM, CRS(projection))
 
@@ -100,8 +103,8 @@ for (lake_day in filenames){
     
     # Make an empty summary table for each filename
     # This will be populated with summary stats for each variable
-    summary_lake<-as.data.frame(matrix(nrow=length(variables), ncol=17))
-    names(summary_lake)<-c('Min', 'Q1', 'Median', 'Mean', 'Q3', 'Max', 'sd', 'n', 'mad', 'MADM', 'skewness', 'loc', 'scale', 'shape', 'CV', 'QuartileDispersion', 'MADMOverMedian')
+    summary_lake<-as.data.frame(matrix(nrow=length(variables), ncol=22))
+    names(summary_lake)<-c('Min', 'Q25', 'Median', 'Mean', 'Q75', 'Max', 'Q05', 'Q10', 'Q90', 'Q95', 'sd', 'SDL', 'n', 'mad', 'MADM', 'skewness', 'loc', 'scale', 'shape', 'CV', 'QuartileDispersion', 'MADMOverMedian')
     
     # ==========================================
     # Start of loop to run through each variable  
@@ -117,27 +120,39 @@ for (lake_day in filenames){
       # Skip variable if all NAs
       if (length(values)>0){
         
-        # Create summary stats for variable
-        basic_stats<-summary(values)
+        if (var %in% minvars[,1]){
+          minimum<-minvars[which(var==minvars[,1]),2]
+          values_plus<-values-minimum+0.1
+        } else {
+          values_plus<-values
+        }
         
-        summary_var<-c(basic_stats, sd=sd(values), n=length(values), mad=mad(values), MADM=median(abs(values-median(values))), skewness=skewness(values, na.rm = T))
+        # Create summary stats for variable
+        basic_stats<-summary(values_plus)
+        quantiles<-quantile(values_plus, probs = c(0.05, .1, .9, 0.95),  na.rm = TRUE)
+        
+        summary_var<-c(basic_stats, quantiles, sd=sd(values_plus, na.rm=T), SDL=sd(log10(values_plus), na.rm=T), n=length(values_plus), mad=mad(values_plus), MADM=median(abs(values_plus-median(values_plus))), skewness=skewness(values_plus, na.rm = T))
+
         
         # Save summary info to summary table
-        summary_lake[var_number,1:11]<-summary_var
+        summary_lake[var_number,1:16]<-summary_var
         
         #If zero heterogeneity skip evd and histogram
-        if (identical(round(min(values), 3), round(max(values,3)))==FALSE){
-          hist(values,breaks=20, xlab=var, main="", col="grey")
-          evd<-fgev(values, std.err=F)
+        if (identical(round(min(values_plus), 3), round(max(values_plus,3)))==FALSE){
+          # hist(values_plus,breaks=20, xlab=var, main="", col="grey")
+          evd<-fgev(values_plus, std.err=F)
           evd$estimate
-          summary_lake[var_number,12:14]<-evd$estimate
+          summary_lake[var_number,17:19]<-evd$estimate
         }
       }
     }
     
     summary_lake$CV<-summary_lake$sd/summary_lake$Mean
-    summary_lake$QuartileDispersion<-(summary_lake$Q3 - summary_lake$Q1)/ (summary_lake$Q3 + summary_lake$Q1)
+    summary_lake$QuartileDispersion<-(summary_lake$Q75 - summary_lake$Q25)/ (summary_lake$Q75 + summary_lake$Q25)
     summary_lake$MADMOverMedian<-(summary_lake$MADM)/ (summary_lake$Median)
+    summary_lake$MaxMinusMin<-(summary_lake$Max) - (summary_lake$Min)
+    summary_lake$IQR<-(summary_lake$Q75) - (summary_lake$Q25)
+    summary_lake$Q95MinusQ05<-(summary_lake$Q95) - (summary_lake$Q05)
     
     summary_lake$Variable<-variables
     
