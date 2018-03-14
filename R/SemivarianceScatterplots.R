8438304c0488d8bf325e2d82474c03225ca6c9b9
lukeloken <lukeloken@gmail.com>
2018-03-14 09:20:27 -0500

    boxplots of chlra ranges vs predictor variables

--- R/SemivarianceScatterplots.R
+++ R/SemivarianceScatterplots.R
@@ -37,6 +37,10 @@ x<-seq(0,max(a$lake_area_ha, na.rm=T)*1.05)
 y<-sqrt(x*10000)
 
 b$Colour <- as.character(cut(b$tp, breaks = seq(0,60,10), labels = brewer.pal(9, "PuBuGn")[3:8]))
+b$pch <- cut(b$lake_area_ha, breaks=c(0,10,20,40,80,160,320,640,2000), labels=seq(.5,4,.5))
+
+b$pch <- as.numeric(as.character(b$pch))
+b$pch[which(is.na(b$pch))] <- 1
 
 #Semivariogram range boxplots
 png("Figures/ScatterplotAreaTempRange.png", res=200, width=4,height=4, units="in")
@@ -111,3 +115,58 @@ mtext('Chl a semivariogram range (m)',2,2)
 
 dev.off()
 
+
+#Semivariogram range boxplots
+png("Figures/ScatterplotTPchlaRange_withcutoff.png", res=200, width=4,height=4, units="in")
+
+par(mar=c(3,3,.5,.5))
+par(mgp=c(3,.5,0), tck=-.02)
+
+plot(b$tp, b$ChlARFU_h, ylim=c(0,max(y)*1.1), pch=16, xlab='', ylab='', col=b$Colour)
+mtext('TP (ug/L)',1,2)
+mtext('Chl a semivariogram range (m)',2,2)
+
+dev.off()
+
+
+#Semivariogram range boxplots
+png("Figures/ScatterplotDOCchlaRange_withcutoff.png", res=200, width=4,height=4, units="in")
+
+par(mar=c(3,3,.5,.5))
+par(mgp=c(3,.5,0), tck=-.02)
+
+plot(b$doc, b$ChlARFU_h, ylim=c(0,max(y)*1.1), pch=16, xlab='', ylab='', col=b$Colour)
+mtext('TP (ug/L)',1,2)
+mtext('Chl a semivariogram range (m)',2,2)
+
+dev.off()
+
+
+
+#Semivariogram range boxplots
+png("Figures/ScatterplotTPTempoverchlaRange_withcutoff.png", res=200, width=4,height=4, units="in")
+
+par(mar=c(3,3,.5,.5))
+par(mgp=c(3,.5,0), tck=-.02)
+
+plot(b$tp, b$TmpC_h/b$ChlARFU_h, col=b$Colour, pch=16, cex=b$pch*.75, xlab='', ylab='')
+abline(h=1, lty=3)
+
+mtext('TP (ug/L)',1,2)
+mtext('Temp/Chl a semivariogram range',2,2)
+
+dev.off()
+
+#Semivariogram range boxplots
+png("Figures/ScatterplotAreaTempoverchlaRange_withcutoff.png", res=200, width=4,height=4, units="in")
+
+par(mar=c(3,3,.5,.5))
+par(mgp=c(3,.5,0), tck=-.02)
+
+plot(b$lake_area_ha, b$TmpC_h/b$ChlARFU_h, col=b$Colour, cex=b$pch*.75, pch=16, xlab='', ylab='')
+abline(h=1, lty=3)
+
+mtext('Lake area (ha)',1,2)
+mtext('Temp/Chl a semivariogram range',2,2)
+
+dev.off()
