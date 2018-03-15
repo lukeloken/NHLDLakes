8e617d3916d6ef6796fb9244ebafc9cf4d8d20bf
lukeloken <lukeloken@gmail.com>
2018-03-15 11:34:20 -0500

    remove code that is unecessary for this script

--- R/CompareSpatialHeterogeneity_v2.R
+++ R/CompareSpatialHeterogeneity_v2.R
@@ -16,7 +16,6 @@ setwd("E:/Git_Repo/NHLDLakes")
 
 j <- readRDS(file='Data/FlameStatsLagosChemAllWide.rds')
 
-#Still need to clean this part up.
 #Subset or select which columns to use...
 goodvars<-c("TempC", "SPCuScm", "fDOMRFU", "TurbFNU", "pH", "ODOmgL",  "CO2uM", "CH4uM", "ChlARFU", "BGAPCRFU")
 shortnames<-c("Temp", "SPC", "fDOM", "Turb", "pH", "DO", "CO2", "CH4", "ChlA", "BGA")
@@ -30,10 +29,6 @@ SemiVars <- c('TmpC_h', 'SPCScm_h', 'fDOMRFU_h', 'TrbFNU_h', 'pH_h', 'ODOmgL_h',
 
 SemiRange_columns<-paste(SemiVars, 'points', 'SemiRange', sep='_')
 
-#Only use tau data values
-tauvars<-c("TempC_t", "SPCScm_t", "fDOMRFU_t", "TrbFNU_t", "pH_tau", "ODOmgL_t",  "CO2uM_t", "CH4uM_t", "ChlARFU_t", "BGAPCRFU_t")
-
-variabletype<-c(1, 1, 1, 1, 2, 2, 2, 2, 3, 3)
 
 # ##############
 # plotting
@@ -131,103 +126,5 @@ abline(h=0, lty=3)
 mtext('Variable', 1, 0, outer=T)
 
 dev.off()
-
-
-
-
-
-# ########################################
-# Compare Lagos Data to Spatial statistics
-# ########################################
-
-data_short2<-left_join(lagos_subset, merged_short, by = c("flame_name" = "Lake"))
-data_CV <-data_short2 %>%
-  dplyr::filter(Statistic =='CV')
-data_QuartileDispersion <-data_short2 %>%
-  dplyr::filter(Statistic =='QuartileDispersion')
-data_skewness <-data_short2 %>%
-  dplyr::filter(Statistic =='skewness')
-data_minmax <-data_short2 %>%
-  dplyr::filter(Statistic =='MaxMinusMin')
-
-
-plot(data_CV$lake_area_ha, data_CV$TempC)
-plot(data_CV$lake_area_ha, data_CV$SPCuScm)
-plot(data_CV$lake_area_ha, data_CV$fDOMRFU)
-plot(data_CV$lake_area_ha, data_CV$TurbFNU)
-plot(data_CV$lake_area_ha, data_CV$pH)
-plot(data_CV$lake_area_ha, data_CV$ODOmgL)
-plot(data_CV$lake_area_ha, data_CV$ChlARFU)
-plot(data_CV$lake_area_ha, data_CV$BGAPCRFU)
-
-plot(data_skewness$lake_area_ha, data_skewness$TempC)
-plot(data_skewness$lake_area_ha, data_skewness$SPCuScm)
-plot(data_skewness$lake_area_ha, data_skewness$fDOMRFU)
-plot(data_skewness$lake_area_ha, data_skewness$TurbFNU)
-plot(data_skewness$lake_area_ha, data_skewness$pH)
-plot(data_skewness$lake_area_ha, data_skewness$ODOmgL)
-plot(data_skewness$lake_area_ha, data_skewness$ChlARFU)
-plot(data_skewness$lake_area_ha, data_skewness$BGAPCRFU)
-
-plot(data_skewness$ShorelineIndex, data_skewness$TempC)
-plot(data_skewness$ShorelineIndex, data_skewness$SPCuScm)
-plot(data_skewness$ShorelineIndex, data_skewness$fDOMRFU)
-plot(data_skewness$ShorelineIndex, data_skewness$TurbFNU)
-plot(data_skewness$ShorelineIndex, data_skewness$pH)
-plot(data_skewness$ShorelineIndex, data_skewness$ODOmgL)
-plot(data_skewness$ShorelineIndex, data_skewness$ChlARFU)
-plot(data_skewness$ShorelineIndex, data_skewness$BGAPCRFU)
-
-plot(data_QuartileDispersion$lake_area_ha, data_QuartileDispersion$TempC)
-plot(data_QuartileDispersion$lake_area_ha, data_QuartileDispersion$SPCuScm)
-plot(data_QuartileDispersion$lake_area_ha, data_QuartileDispersion$fDOMRFU)
-plot(data_QuartileDispersion$lake_area_ha, data_QuartileDispersion$TurbFNU)
-plot(data_QuartileDispersion$lake_area_ha, data_QuartileDispersion$pH)
-plot(data_QuartileDispersion$lake_area_ha, data_QuartileDispersion$ODOmgL)
-plot(data_QuartileDispersion$lake_area_ha, data_QuartileDispersion$ChlARFU)
-plot(data_QuartileDispersion$lake_area_ha, data_QuartileDispersion$BGAPCRFU)
-
-
-plot(data_QuartileDispersion$ShorelineIndex, data_QuartileDispersion$TempC)
-plot(data_QuartileDispersion$ShorelineIndex, data_QuartileDispersion$SPCuScm)
-plot(data_QuartileDispersion$ShorelineIndex, data_QuartileDispersion$fDOMRFU)
-plot(data_QuartileDispersion$ShorelineIndex, data_QuartileDispersion$TurbFNU)
-plot(data_QuartileDispersion$ShorelineIndex, data_QuartileDispersion$pH)
-plot(data_QuartileDispersion$ShorelineIndex, data_QuartileDispersion$ODOmgL)
-plot(data_QuartileDispersion$ShorelineIndex, data_QuartileDispersion$ChlARFU)
-plot(data_QuartileDispersion$ShorelineIndex, data_QuartileDispersion$BGAPCRFU)
-
-
-plot(data_minmax$lake_area_ha, data_minmax$TempC)
-plot(data_minmax$lake_area_ha, data_minmax$SPCuScm)
-plot(data_minmax$lake_area_ha, data_minmax$fDOMRFU)
-plot(data_minmax$lake_area_ha, data_minmax$TurbFNU)
-plot(data_minmax$lake_area_ha, data_minmax$pH)
-plot(data_minmax$lake_area_ha, data_minmax$ODOmgL)
-plot(data_minmax$lake_area_ha, data_minmax$ChlARFU)
-plot(data_minmax$lake_area_ha, data_minmax$BGAPCRFU)
-
-
-
-# ################################
-# Make wide and long tables
-# ################################
-
-data_long<-gather(data_short, key='stat', value='value', 1:17) 
-
-data_wide<- data_short %>% 
-  dplyr::select(-c(VariableFactor)) %>%
-  gather(key=stat, value=value, 1:17) %>%
-  unite(temp, Variable, stat) %>%
-  spread(temp, value)
-
-plot(data_wide$TempC_CV, data_wide$ChlARFU_CV)
-abline(0,1)
-
-plot(data_wide$SPCuScm_CV, data_wide$ChlARFU_CV)
-abline(0,1)
-
-plot(data_wide$TurbFNU_CV, data_wide$ChlARFU_CV)
-abline(0,1)
 
 
