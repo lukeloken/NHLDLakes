acaaf3ea164d7496a89c1ba03dd41d8e312a74e7
lukeloken <lukeloken@gmail.com>
2018-03-23 11:18:46 -0500

    Give MidgeLake a watershed area of 10 ha, this is based on imagery, but is kinda random.
    Also calculate stream density metrics and ratios

--- R/MergeFlameSpatialwithLagosChem.R
+++ R/MergeFlameSpatialwithLagosChem.R
@@ -5,6 +5,14 @@ names(MyChemLagos)[1]<-'Lake'
 
 e<-MyChemLagos[,c(1, 14:22, 57, 62:63, 78, 102:103, 105,191,192)]
 
+#Create additional metrics describing watershed/stream attributes compared to lake size
+e$iws_ha[e$Lake=='MidgeLake']<-10
+e$streamlength_to_lakearea<-e$iws_streamdensity_streams_sum_lengthm/e$lake_area_ha
+e$streamdensity_to_lakearea<-e$iws_streamdensity_streams_density_mperha/e$lake_area_ha
+
+e$iwsarea_to_lakearea <- e$iws_ha/e$lake_area_ha
+
+
 #Long Table (one row = one value), values are by lake, date, Geotype, variable, stat
 d<- readRDS(file='Data/FlameStatsAll.rds')
 
