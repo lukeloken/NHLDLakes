18ed005be8ae0e7b0b78ed2a43e9b5649cd92319
lukeloken <lukeloken@gmail.com>
2018-03-29 18:27:54 -0500

    added abbreviations to spatial summary table

--- R/CalculateSpatialSummaries.R
+++ R/CalculateSpatialSummaries.R
@@ -13,15 +13,18 @@ j <- readRDS(file='Data/FlameStatsLagosChemAllWide.rds')
 
 #Get Lagos data
 # mylagos2<-readRDS(file='Data/MyLakesLagos.rds')
-morphvars<-c("lake_area_ha", "lake_perim_meters", "ShorelineIndex", "maxdepth", "iws_ha", "iws_streamdensity_streams_sum_lengthm")
-morphnames<-c('Lake area', 'Lake perimeter', 'Shoreline Index', 'Max depth', 'Watershed area', 'Total stream length')
+morphvars<-c("lake_area_ha", "lake_perim_meters", "ShorelineIndex", "maxdepth", "iws_ha", 'iwsarea_to_lakearea', "iws_streamdensity_streams_sum_lengthm", 'streamlength_to_lakearea', 'lakeconnection')
+morphnames<-c('Lake area', 'Lake perimeter', 'Shoreline development index', 'Max depth', 'Watershed area', 'Watershed area to lake area', 'Total stream length', 'Total stream length to lake area', 'Lake connection')
+morphabbr<-c('LA', 'LP', 'SDI', 'Z', 'WA', 'WA:LA', 'SL', 'SL:LA', 'LC')
 
 chemvars<-c('TotalPUF', "TotalNUF", "SRP", "NO3NO2", "NH4", 'DOC', 'Cl')
-chemnames<-c('Total P', 'Total N', 'SRP', 'NO3-N', 'NH4-N', 'DOC', 'CL')
+chemnames<-c('Total P', 'Total N', 'SRP', 'NO3-N', 'NH4-N', 'DOC', 'Chloride')
+chemabbr<-c('TP', 'TN', 'SRP', 'NO3', 'NH4', 'DOC', 'Cl')
+
 
 goodvars<-c("TempC", "SPCuScm", "fDOMRFU", "TurbFNU", "pH", "ODOmgL",  "CO2uM", "CH4uM", "ChlARFU", "BGAPCRFU")
 shortnames<-c("Temp", "SPC", "fDOM", "Turb", "pH", "DO", "CO2", "CH4", "ChlA", "BGA")
-
+longnames<-c('Temperature', 'Specific conductivity', 'Fluorescent dissolved organic matter', 'Turbidity', 'pH', 'Dissolved oxygen', 'Carbon dioxide', 'Methane', 'Chlorophyll A', 'Blue green algae/phycocyanin')
 goodvars_points<-paste(goodvars, 'points', sep='_')
 
 flamevars<-paste(goodvars, 'pixels', 'Median', sep='_')
@@ -45,15 +48,17 @@ means<- table_subset %>%
 table_bound <- bind_rows(medians, means, mins, maxs)
 table_bound$lake_perim_meters<-table_bound$lake_perim_meters/1000
 table_bound$iws_streamdensity_streams_sum_lengthm<-table_bound$iws_streamdensity_streams_sum_lengthm/1000
-names(table_bound)<-c(morphnames, chemnames, shortnames)
+names(table_bound)<-c(morphnames, chemnames, longnames)
 
 table_out<-as.data.frame(t(table_bound))
 names(table_out)<-c('Median', 'Mean', 'Min', 'Max')
 table_out$Variable<-row.names(table_out)
+table_out$Abbr<-c(morphabbr, chemabbr, shortnames)
+
 row.names(table_out)<-NULL
 
 table_out[,1:4]<-apply(table_out[,1:4], 2, round, digits=2)
-table_out<-table_out[,c(5,1:4)]
+table_out<-table_out[,c(5:6,1:4)]
 
 
 write.table(table_out, file='Data/AcrossLakeSummary.csv', row.names=F, sep=',')
