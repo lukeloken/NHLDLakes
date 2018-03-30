5f9db6183971fc483f4969795d2559b640f4e7b6
lukeloken <lukeloken@gmail.com>
2018-03-30 12:50:38 -0500

    Code now selects spatial stat variables at the start and used 'VarStat' throughout to label statistics. The object name still retains sd in the object name (e.g., sd_columns), but this can refer to any set of values from the wide datatable

--- R/sd_MLR.R
+++ R/sd_MLR.R
@@ -1,5 +1,11 @@
 
+# Script to loop through standard deviations within lakes
+# Pull out lake scale predictor variables. 
+# Currently using bestglm and random forest
+# Outputs a bunch of figures (scatterplots, model outputs)
+# Still needs to be ruggedized to convert to CV and other metrics
 
+# Not sure what all needs to be loaded
 library(RColorBrewer)
 library(dplyr)
 library(tidyr)
@@ -17,47 +23,63 @@ library(randomForestSRC)
 library(ggRandomForests)
 library(grid)
 
+#Wide table
 j <- readRDS(file='Data/FlameStatsLagosChemAllWide.rds')
 
+#Color scheme to be used for all plotting
 colors<-c('#377eb8', '#e41a1c', '#4daf4a')
 colorbyvar<-colors[c(1,1,1,1,2,2,2,2,3,3)]
 
+#Spatial heterogeneiety stat to use
+VarStat <-'sd'
+
+#Vectors of names
 goodvars<-c("TempC", "SPCuScm", "fDOMRFU", "TurbFNU", "pH", "ODOmgL",  "CO2uM", "CH4uM", "ChlARFU", "BGAPCRFU")
 shortnames<-c("Temp", "SPC", "fDOM", "Turb", "pH", "DO", "CO2", "CH4", "ChlA", "BGA")
 
 mediannames<-paste0(goodvars, 'Median')
 goodvars_points<-paste(goodvars, 'points', sep='_')
 
-
-sd_columns<-paste(goodvars, 'points', 'sd', sep='_')
+sd_columns<-paste(goodvars, 'points', VarStat, sep='_')
 mean_columns<-paste(goodvars, 'points', 'Mean', sep='_')
 
-
-sd_columns_pix<-paste(goodvars, 'pixels', 'sd', sep='_')
+sd_columns_pix<-paste(goodvars, 'pixels', VarStat, sep='_')
 med_columns_pix<-paste(goodvars, 'pixels', 'Median', sep='_')
 
+# Which predictor variables to use from flame medians
+# Omit fDOM and pH as these have multicollinearity issues
+# Omit methane and CO2 since not available for all lakes
 flamepredvars<-med_columns_pix[c(1,2,4,6,9,10)]
 
 #only use lakes with lake area
 k_full<-j
+
 #Change lake connection to numeric (1 = higher landscape position)
 connect_df<-data.frame(lakeconnection=c('Isolated', 'Headwater', 'DR_Stream', 'DR_LakeStream'), value=1:4)
 k_full$lakeconn <- as.numeric(as.character(connect_df$value[match(k_full$lakeconnection, connect_df$lakeconnection)]))
 
-# predvars<-names(k)[c(2,4,6:17)]
+#Omit Laura Lake as this lake was clearly two separated lakes
+k_full<-k_full[which(k_full$Lake!='LauraLake'),]
+
+# List of useful predictor variables
 predvars_full<-c(names(k_full)[c(2,6, 10, 12,15,16,17:22)], 'lakeconn')
 
+#Subset predictor variables to eliminate multicollinearity issues
 predvars<-predvars_full[c(1:7,10,12,13)]
 
-# Tables of sd, flamepred vars, and morph predvars
+# Tables of variabity stat (e.g., sd), flamepred vars, and morph predvars
 df_sd<-k_full[sd_columns_pix]
-# df_sd<-k_full[sd_columns]
-# df_mean <-k[mean_columns]
 df_median <-k_full[flamepredvars]
 df_morph<-k_full[predvars]
 
-
+# subset to only include lakes will all available variables
+# Note that this table is not used for glm and random forest as these can handle missin values
 k<-na.omit(k_full[c('Lake', predvars, flamepredvars, sd_columns_pix)])
+
+
+
+############ Simple linear model ###########
+# Old way of doing this using all predictors
 
 # set predictor variables (morphometry, and flame variables)
 m1<-k[,predvars[1]]
@@ -65,11 +87,11 @@ m2<-k[,predvars[2]]
 m3<-k[,predvars[3]]
 m4<-k[,predvars[4]]
 m5<-k[,predvars[5]]
-m6<-k[,predvars[length(predvars)]]
+m6<-k[,predvars[length(predvars)]] #lakeconn as numeric
 m7<-k[,predvars[7]]
 m8<-k[,predvars[8]]
 m9<-k[,predvars[9]]
-m10<-k[,predvars[6]]
+m10<-k[,predvars[6]] #lakeconn as factor
 
 
 f1<-k[,flamepredvars[1]]
@@ -79,16 +101,13 @@ f4<-k[,flamepredvars[4]]
 f5<-k[,flamepredvars[5]]
 f6<-k[,flamepredvars[6]]
 
-#set y value (Start with Temp and loop through all CV vars)
-
+# Loop through 10 y variables (sdTemp, sdSPC, etc.)
 modellist<-list()
 column<-1
 for (column in 1:length(sd_columns_pix)) {
 y<-k[,sd_columns_pix[column]]
 
 fvars<-c(1:length(flamepredvars))
-
-# fvars <-fvars[which(fvars!=column)]
 
 xvars<-paste(paste('m', c(1:9), sep='', collapse='+'), paste('f', fvars, sep='', collapse="+"), sep="+")
 
@@ -109,24 +128,33 @@ modellist[[column]] <-stepmodel
 
 modellist
 
+#print models
 model<-1
 for (model in 1:length(modellist)){
+  print(sd_columns_pix[model])
   summary<-summary(modellist[[model]])
   print(summary)
-  print(sd_columns_pix[model])}
+}
 
 
+############ Best GLM and RandomForest###########
+# Better way of doing this using all predictors
+# Code uses k_full which contains NAs
+# loops through all response variables and generates a bestglm
+# and a random forest model
 
-#Best glm model of sd
+#Create empty lists to populate
 glmlist<-list()
 forest_fit<-list()
 rfsrc_fit<-list()
-column<-3
+
+#Start loop
+column<-1
 for (column in 1:ncol(df_sd)){
   
   name<-names(df_sd)[column]
-  name_short<-paste0('Drivers of within-lake standard deviation of ', shortnames[column])
-  df<-data.frame( df_median[which(k_full$Lake!='LauraLake'),], df_morph[which(k_full$Lake!='LauraLake'),-10], y=df_sd[which(k_full$Lake!='LauraLake'),column])
+  name_short<-paste0('Drivers of within-lake ', VarStat, ' of ', shortnames[column])
+  df<-data.frame( df_median, df_morph[,-10], y=df_sd[,column])
   # df<-data.frame(df_morph[-which(k$Lake=='LauraLake'),-c(6,8,9,11)], y=df_sd[-which(k$Lake=='LauraLake'),column])
   # df2<-data.frame( df_morph[,-c(6,8,9,11)], y=df_sd[,column])
 
@@ -144,11 +172,11 @@ for (column in 1:ncol(df_sd)){
   
   rfsrc_fit[[column]] <- rfsrc(y~., data=df_noNA)
   
-  png(paste0("Figures/RandomForest/RF_", name, ".png"), res=200, width=8,height=6, units="in")
+  png(paste0("Figures/RandomForest/", VarStat, "/RF_", VarStat, name,  ".png"), res=200, width=8,height=6, units="in")
   print(randomForest::varImpPlot(forest_fit[[column]], main=name_short))
   dev.off()
   
-  png(paste0("Figures/RandomForest/GG_", name, ".png"), res=200, width=8,height=6, units="in")
+  png(paste0("Figures/RandomForest/", VarStat, "/GG_", VarStat, name,  ".png"), res=200, width=8,height=6, units="in")
   RF1<- plot(gg_vimp(rfsrc_fit[[column]], nvar=10)) +
     theme_bw() +
     theme(legend.position="none") +
@@ -183,7 +211,7 @@ lapply(lmlist, function(l) summary(l))
 lapply(lmlist, function(l) anova(l))
 
 
-png(paste0("Figures/GLMModel/GLM_sd.png"), res=200, width=4.5,height=10, units="in")
+png(paste0("Figures/GLMModel/GLM_", VarStat, ".png"), res=200, width=4.5,height=10, units="in")
 
 par(mar=c(2,2,.5,.5), oma=c(1.5,1.5,1.5,0))
 par(mgp=c(2, .5, 0))
@@ -200,31 +228,14 @@ legend('bottomright', inset=0.01, c(paste0('r2=', (r2))), bty='n')
 }
 mtext('Predicted standard deviation', 2, 0, outer=T)
 mtext('Observed standard deviation', 1, 0, outer=T)
-mtext('Best GLM for predicting within lake standard deviation', 3, 0, outer=T)
+mtext(paste0('Best GLM for predicting within lake ', VarStat), 3, 0, outer=T)
 
 
 dev.off()
 
 
 
-# Coefs<-lapply(glmlist, function(l) l[[1]]['coefficients'][[1]])
-# Vars<-lapply(Coefs, function(l) names(l)[-1])
-# VarTable<-as.data.frame(sapply(Vars, '[', seq(max(sapply(Vars, length)))), stringsAsFactors=F)
-# names(VarTable)<-shortnames
-# 
-# VarTable[grep('LC', VarTable[,2]),2] <- 'LC'
-# VarTable[grep('LC', VarTable[,8]),8] <- 'LC'
-# 
-# VarTable
-# 
-# column<-1
-# for (column in 1:nrow(VarTable)){
-#   y<-df_sd[-which(k$Lake=='LauraLake'),column]
-#   
-# }
-
-
-png(paste0("Figures/RandomForest/GG_AllVars.png"), res=200, width=8,height=10, units="in")
+png(paste0("Figures/RandomForest/", VarStat, "/GG_", VarStat, "_AllVars.png"), res=200, width=8,height=10, units="in")
 
 RFplots<-list()
 for (column in 1:length(rfsrc_fit)){
@@ -241,7 +252,7 @@ RFplots[[column]]<- plot(gg_vimp(rfsrc_fit[[column]], nvar=8)) +
 }
 
 grid.arrange(RFplots[[1]], RFplots[[2]], RFplots[[3]],RFplots[[4]],RFplots[[5]],RFplots[[6]],RFplots[[7]],RFplots[[8]],RFplots[[9]],RFplots[[10]], 
-             top=textGrob("Random forest top predictors of within lake standard deviation of",
+             top=textGrob(paste0("Random forest top predictors of within lake ", VarStat, " of"),
                           gp=gpar(fontsize=10)),
              bottom=textGrob("Variable importance", gp=gpar(fontsize=10)),
              left=textGrob("Predictor variable", gp=gpar(fontsize=10), rot=90),
@@ -252,7 +263,7 @@ dev.off()
 
 # plot(gg_minimal_vimp(rfsrc_fit[[column]]))
 
-png(paste0("Figures/Boxplots/sd_lakeconnection_points.png"), res=200, width=4.5,height=10, units="in")
+png(paste0("Figures/Boxplots/", VarStat, "_lakeconnection_points.png"), res=200, width=4.5,height=10, units="in")
 
 par(mfrow=c(5,2))
 par(mar=c(1.5,2,1.5,.5), oma=c(1.5,2,1,0))
@@ -267,12 +278,12 @@ boxplot(y ~ k_full$lakeconn, ylim=c(0,ymax[var_nu]), col=viridis(4), boxwex=0.5)
 legend('top', inset=0, shortnames[var_nu], bty='n', xjust=0.5, x.intersp=0)
 }
 mtext('Lake Connection', 1,0,outer=T)
-mtext('Within-lake standard deviation', 2,0,outer=T)
+mtext(paste0('Within-lake ', VarStat), 2,0,outer=T)
 mtext('Points', 3,-1,outer=T)
 
 dev.off()
 
-png(paste0("Figures/Boxplots/sd_lakeconnection_pixels.png"), res=200, width=4.5,height=10, units="in")
+png(paste0("Figures/Boxplots/", VarStat, "_lakeconnection_pixels.png"), res=200, width=4.5,height=10, units="in")
 
 par(mfrow=c(5,2))
 par(mar=c(1.5,2,1.5,.5), oma=c(1.5,2,1,0))
@@ -287,7 +298,7 @@ for (var_nu in 1:length(sd_columns_pix)){
   legend('top', inset=0, shortnames[var_nu], bty='n', xjust=0.5, x.intersp=0)
 }
 mtext('Lake Connection', 1,0,outer=T)
-mtext('Within-lake standard deviation', 2,0,outer=T)
+mtext(paste0('Within-lake ', VarStat), 2,0,outer=T)
 mtext('Pixels', 3,-1,outer=T)
 
 dev.off()
@@ -299,7 +310,7 @@ for (xvar in 1:length(predvars)){
   x<-df_morph[,predvars[xvar]]
   if (class(x)=='numeric'){
   
-  png(paste0("Figures/Scatterplots/sd_by_predictor/sd_", predvars[xvar], "_points.png"), res=200, width=4.5,height=10, units="in")
+  png(paste0("Figures/Scatterplots/", VarStat, "_by_predictor/", VarStat, "_", predvars[xvar], "_points.png"), res=200, width=4.5,height=10, units="in")
   
   par(mfrow=c(5,2))
   par(mar=c(1.5,2,1.5,.5), oma=c(1.5,2,2,0))
@@ -314,12 +325,12 @@ for (xvar in 1:length(predvars)){
     mtext(shortnames[var_nu], 3,0)
   }
   mtext(predvars[xvar], 1,0,outer=T)
-  mtext('Within-lake standard deviation', 2,0,outer=T)
+  mtext(paste0('Within-lake ', VarStat), 2,0,outer=T)
   mtext('Points', 3,-1,outer=T)
   
   dev.off()
   
-  png(paste0("Figures/Scatterplots/sd_by_predictor/sd_", predvars[xvar], "_pixels.png"), res=200, width=4.5,height=10, units="in")
+  png(paste0("Figures/Scatterplots/", VarStat, "_by_predictor/", VarStat, "_", predvars[xvar], "_pixels.png"), res=200, width=4.5,height=10, units="in")
   
   par(mfrow=c(5,2))
   par(mar=c(1.5,2,1.5,.5), oma=c(1.5,2,2,0))
@@ -334,7 +345,7 @@ for (xvar in 1:length(predvars)){
     mtext(shortnames[var_nu], 3,0)
   }
   mtext(predvars[xvar], 1,0,outer=T)
-  mtext('Within-lake standard deviation', 2,0,outer=T)
+  mtext(paste0('Within-lake ', VarStat), 2,0,outer=T)
   mtext('Pixels', 3,-1,outer=T)
   
   dev.off()
@@ -353,7 +364,7 @@ for (var_nu in 1:length(sd_columns)){
   y<-k_full[,sd_columns[var_nu]]
   name<-shortnames[var_nu]
   
-  png(paste0("Figures/Scatterplots/sd_by_response/sd_", name, "_points.png"), res=200, width=6.5,height=12, units="in")
+  png(paste0("Figures/Scatterplots/", VarStat, "_by_response/", VarStat, "_", name, "_points.png"), res=200, width=6.5,height=12, units="in")
   
   par(mfrow=c(5,3))
   par(mar=c(1.5,2,1.5,.5), oma=c(2,2,2,0))
@@ -368,14 +379,14 @@ for (var_nu in 1:length(sd_columns)){
       mtext(xvars[xvar], 1,1.5)
     }
   }
-    mtext(paste0('Within lake standard deviation of ', name), 2,0,outer=T)
+    mtext(paste0('Within lake ', VarStat, ' of ', name), 2,0,outer=T)
     mtext(paste0(name, ' Points'), 3,-1,outer=T)
     
     dev.off()
     
     y2<-k_full[,sd_columns_pix[var_nu]]
     
-    png(paste0("Figures/Scatterplots/sd_by_response/sd_", name, "_pixels.png"), res=200, width=6.5,height=12, units="in")
+    png(paste0("Figures/Scatterplots/", VarStat, "_by_response/", VarStat, "_", name, "_pixels.png"), res=200, width=6.5,height=12, units="in")
     
     par(mfrow=c(5,3))
     par(mar=c(1.5,2,1.5,.5), oma=c(2,2,2,0))
@@ -389,7 +400,7 @@ for (var_nu in 1:length(sd_columns)){
         mtext(xvars[xvar], 1,1.5)
       }
     }
-    mtext(paste0('Within lake standard deviation of ', name), 2,0,outer=T)
+    mtext(paste0('Within lake ', VarStat, ' of ', name), 2,0,outer=T)
     mtext(paste0(name, ' Pixels'), 3,-1,outer=T)
     
     dev.off()
