05c7586da5b669c522deedf5db5b88fd593283d3
lukeloken <lukeloken@gmail.com>
2018-03-31 11:24:04 -0500

    Functions to pull out linear models from GLM outputs and plot GLM predictions vs observations

---
+++ R/GLMcustom.R
@@ -0,0 +1,17 @@
+
+
+#### Functions to pull out and plot glm objects ####
+glm2lm<-function(glm){
+  df<-glm[[1]]$model
+  equation<-paste0(names(df)[1], '~', paste(names(df)[2:ncol(df)], collapse='+'))
+  lm<-lm(equation, data=df)
+  return(lm)
+}
+
+
+glmplot<-function (glm, ...){
+  y_obs<-glm[[1]]$model[,1]
+  y_pred<-predict(glm$BestModel, glm[[1]]$model)
+  plotlim<-range(c(y_obs, y_pred), na.rm=T)
+  plot(y_obs, y_pred, xlim=plotlim, ylim=plotlim,...)
+}
