c9a4b8916c76f16d4836447e2fd80a1c2aa818c4
lukeloken <lukeloken@gmail.com>
2018-06-07 15:07:30 -0500

    added another way to visualize sd within and among lakes

--- R/CalculateSpatialSummaries.R
+++ R/CalculateSpatialSummaries.R
@@ -26,12 +26,14 @@ chemnames<-c('Total P', 'Total N', 'SRP', 'NO3-N', 'NH4-N', 'DOC', 'Chloride')
 chemabbr<-c('TP', 'TN', 'SRP', 'NO3', 'NH4', 'DOC', 'Cl')
 
 
-goodvars<-c("TempC", "SPCuScm", "fDOMRFU", "TurbFNU", "pH", "ODOmgL",  "CO2uM", "CH4uM", "ChlARFU", "BGAPCRFU")
-shortnames<-c("Temp", "SPC", "fDOM", "Turb", "pH", "DO", "CO2", "CH4", "ChlA", "BGA")
-longnames<-c('Temperature', 'Specific conductivity', 'Fluorescent dissolved organic matter', 'Turbidity', 'pH', 'Dissolved oxygen', 'Carbon dioxide', 'Methane', 'Chlorophyll A', 'Blue green algae/phycocyanin')
+goodvars<-c("TempC", "SPCuScm", "fDOMRFU", "pH", "TurbFNU", "ODOmgL",  "CO2uM", "CH4uM", "ChlARFU", "BGAPCRFU")
+shortnames<-c("Temp", "SPC", "fDOM",  "pH", "Turb", "DO", "CO2", "CH4", "ChlA", "BGA")
+longnames<-c('Temperature', 'Specific conductivity', 'Fluorescent dissolved organic matter', 'pH', 'Turbidity', 'Dissolved oxygen', 'Carbon dioxide', 'Methane', 'Chlorophyll A', 'Blue green algae/phycocyanin')
 goodvars_points<-paste(goodvars, 'points', sep='_')
 
 flamevars<-paste(goodvars, 'pixels', 'Median', sep='_')
+flamevars_sd<-paste(goodvars, 'pixels', 'sd', sep='_')
+
 
 table_subset <- j %>%
   group_by(Lake) %>%
@@ -52,6 +54,11 @@ means<- table_subset %>%
 sds<- table_subset %>%
   dplyr::summarize_at(c(morphvars, chemvars, flamevars), sd, na.rm=T)
 
+table_subset_sd <- j %>%
+  group_by(Lake) %>%
+  dplyr::summarize_at(c(flamevars_sd), median, na.rm=T) %>%
+  dplyr::summarize_at(c(flamevars_sd), median, na.rm=T)
+
 table_bound <- bind_rows(medians, means, mins, maxs, sds)
 table_bound$lake_perim_meters<-table_bound$lake_perim_meters/1000
 table_bound$iws_streamdensity_streams_sum_lengthm<-table_bound$iws_streamdensity_streams_sum_lengthm/1000
@@ -70,5 +77,43 @@ table_out<-table_out[,c(6:7,1:5)]
 
 write.table(table_out, file='Data/AcrossLakeSummary.csv', row.names=F, sep=',')
 saveRDS(table_out, file='Data/AcrossLakeSummary.rds')
+
+
+
+
+sd_table<-data.frame(var1=as.numeric(sds[flamevars]), var2=as.numeric(table_subset_sd))
+sd_table$percentamong<-sd_table$var1/(sd_table$var1+sd_table$var2)
+sd_table$percentwithin<-sd_table$var2/(sd_table$var1+sd_table$var2)
+sd_table$withinoveramong<-sd_table$var2/(sd_table$var1)
+
+  
+barcolors<-c('grey20', 'grey80')
+
+
+png("Figures/sdAmongvsWithinlakes.png", width=5,height=3, units="in", res=400)
+
+par(mfrow=c(1,1))
+par(mar=c(2.5,3,1,1))
+par(mgp=c(3,.5,0), tck=-0.02)
+
+colors<-c('#377eb8', '#e41a1c', '#4daf4a')
+colorbyvar<-colors[c(1,1,1,1,1,2,2,2,2,2)]
+
+# barplot(rep(1,10), las=1, axes=F, names.arg=shortnames, col=barcolors[2], yaxs='i', ylim=c(0,1), cex.names=.7, mgp=c(3,.1,0))
+# barplot(sd_table$percentwithin, add=T, col=barcolors[1], axes=F)
+# barplot(sd_table$withinoveramong, las=1, axes=F, names.arg=shortnames, col=barcolors[1], yaxs='i', ylim=c(0,1), cex.names=.7, mgp=c(3,.1,0))
+barplot(sd_table$withinoveramong, las=1, axes=F, names.arg=shortnames, col=colorbyvar, yaxs='i', ylim=c(0,1), cex.names=.7, mgp=c(3,.1,0))
+
+
+
+axis(2,at=seq(0,1,.2), labels=seq(0,1,.2), las=1)
+mtext('SD within lakes : SD among lakes',2,1.75)
+mtext('Variable',1,1.5)
+
+# legend('topleft', inset=0.02, c('Among lakes*', 'Within lakes'), fill=barcolors[2:1], x.intersp = 0.8)
+
+box(which='plot')
+
+dev.off()
 
 
